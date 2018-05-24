34d06982dda1adc53642f4645d67a940d63aafab
Eric Machado <ericmachadoeng@gmail.com>
2018-05-24 20:22:29 -0300

    Subindo primeira aula em R

---
+++ R Introdução/01-Rstudio.R
@@ -0,0 +1,45 @@
+# Exporando o 01- RStudio.R
+
+#nome dos contribuidores
+contributors()
+
+# Licen?a
+license()
+
+## Informa??es sobre a sess?o
+sessionInfo()
+
+# Imprimir na tela
+print('R - Uma das principais ferramentas do Cientista de Dados')
+'R - Uma das principais ferramentas do Cientista de Dados'
+
+
+# Criar gr?ficos
+plot(1:10)
+hist(rnorm(10))
+
+# Instalar pacotes
+  install.packages('randomForest') # ?rvore de decis?o de Machine Learning
+install.packages('ggplot2')
+# Carregar o pacote
+library(ggplot2)
+
+# Descarregar o pacote
+detach(package : ggplot2)
+
+search()
+
+# Se souber o nome da fun??o
+
+
+help(mean)
+?mean
+
+# Se n?o souber o nome da fun??o
+help.search('randomForest') 
+help.search('matplot')
+?matplot
+RSiteSearch('matplot')
+example('matplot')
+
+
